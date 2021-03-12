f55eabebb80cfd15bf77bd7e63f5b0418f6e4ada
Daniel Garrido <dgarrido@plainconcepts.com>
2021-03-12 15:49:32 +0100

    Update examples

--- examples/virtual-network-peering-with-virtual-gateway/main.tf
+++ examples/virtual-network-peering-with-virtual-gateway/main.tf
@@ -2,44 +2,44 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "tf-az-vnet-peering" {
+resource "azurerm_resource_group" "rg" {
   name     = "terraform-azurerm-virtual-network-peering"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "tf-az-vnet-peering-1" {
-  name                = "terraform-azurerm-virtual-network-peering-vnet1"
-  resource_group_name = azurerm_resource_group.tf-az-vnet-peering.name
-  location            = azurerm_resource_group.tf-az-vnet-peering.location
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "virtual-network-1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.1.0.0/16"]
 }
 
-resource "azurerm_subnet" "tf-az-vnet-peering-vnet1" {
+resource "azurerm_subnet" "subnet" {
   name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_virtual_network.tf-az-vnet-peering-1.resource_group_name
-  virtual_network_name = azurerm_virtual_network.tf-az-vnet-peering-1.name
+  resource_group_name  = azurerm_virtual_network.vnet1.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.1.1.0/24"]
 }
 
-resource "azurerm_virtual_network" "tf-az-vnet-peering-2" {
-  name                = "terraform-azurerm-virtual-network-peering-vnet2"
-  resource_group_name = azurerm_resource_group.tf-az-vnet-peering.name
-  location            = azurerm_resource_group.tf-az-vnet-peering.location
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "virtual_network_1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.2.0.0/16"]
 }
 
-resource "azurerm_public_ip" "tf-az-vnet-peering" {
-  name                = "terraform-azurerm-virtual-network-peering-vpn-pip"
-  resource_group_name = azurerm_resource_group.tf-az-vnet-peering.name
-  location            = azurerm_resource_group.tf-az-vnet-peering.location
+resource "azurerm_public_ip" "pip" {
+  name                = "public_ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 }
 
-resource "azurerm_virtual_network_gateway" "tf-az-vnet-peering" {
-  name                = "terraform-azurerm-virtual-network-peering-vpn"
-  resource_group_name = azurerm_virtual_network.tf-az-vnet-peering-1.resource_group_name
-  location            = azurerm_virtual_network.tf-az-vnet-peering-1.location
+resource "azurerm_virtual_network_gateway" "vgw" {
+  name                = "virtual_network_gateway"
+  resource_group_name = azurerm_virtual_network.rg.resource_group_name
+  location            = azurerm_virtual_network.rg.location
   type                = "Vpn"
   vpn_type            = "RouteBased"
   sku                 = "Basic"
@@ -47,33 +47,35 @@ resource "azurerm_virtual_network_gateway" "tf-az-vnet-peering" {
   ip_configuration {
     name                          = "vnetGatewayConfig"
     private_ip_address_allocation = "Dynamic"
-    subnet_id                     = azurerm_subnet.tf-az-vnet-peering-vnet1.id
-    public_ip_address_id          = azurerm_public_ip.tf-az-vnet-peering.id
+    subnet_id                     = azurerm_subnet.subnet.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
-module "tf-az-vnet-peering-1" {
-  depends_on           = [azurerm_virtual_network_gateway.tf-az-vnet-peering]
-  source               = "./terraform-azurerm-virtual-network-peering"
-  resource_group_name  = azurerm_virtual_network.tf-az-vnet-peering-1.resource_group_name
-  virtual_network_name = azurerm_virtual_network.tf-az-vnet-peering-1.name
+module "peering1" {
+  depends_on           = [azurerm_virtual_network_gateway.vgw]
+  source               = "aztfm/virtual-network-peering/azurerm"
+  version              = ">=1.0.0"
+  resource_group_name  = azurerm_virtual_network.vnet1.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   peerings = [
     {
-      name                      = azurerm_virtual_network.tf-az-vnet-peering-2.name
-      remote_virtual_network_id = azurerm_virtual_network.tf-az-vnet-peering-2.id
+      name                      = azurerm_virtual_network.vnet2.name
+      remote_virtual_network_id = azurerm_virtual_network.vnet2.id
       allow_gateway_transit     = true
     }
   ]
 }
 
-module "tf-az-vnet-peering-2" {
-  source               = "./terraform-azurerm-virtual-network-peering"
-  resource_group_name  = azurerm_virtual_network.tf-az-vnet-peering-2.resource_group_name
-  virtual_network_name = azurerm_virtual_network.tf-az-vnet-peering-2.name
+module "peering2" {
+  source               = "aztfm/virtual-network-peering/azurerm"
+  version              = ">=1.0.0"
+  resource_group_name  = azurerm_virtual_network.vnet2.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
   peerings = [
     {
-      name                      = module.tf-az-vnet-peering-1.peerings[azurerm_virtual_network.tf-az-vnet-peering-2.name].virtual_network_name
-      remote_virtual_network_id = azurerm_virtual_network.tf-az-vnet-peering-1.id
+      name                      = module.peering1.peerings[azurerm_virtual_network.vnet2.name].virtual_network_name
+      remote_virtual_network_id = azurerm_virtual_network.vnet1.id
       use_remote_gateways       = true
     }
   ]
--- examples/virtual-network-with-multi-peering/main.tf
+++ examples/virtual-network-with-multi-peering/main.tf
@@ -2,58 +2,61 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "tf-az-vnet-peering" {
+resource "azurerm_resource_group" "rg" {
   name     = "terraform-azurerm-virtual-network-peering"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "tf-az-vnet-peering-1" {
+resource "azurerm_virtual_network" "vnet1" {
   name                = "terraform-azurerm-virtual-network-peering-vnet1"
-  resource_group_name = azurerm_resource_group.tf-az-vnet-peering.name
-  location            = azurerm_resource_group.tf-az-vnet-peering.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.1.0.0/16"]
 }
 
-resource "azurerm_virtual_network" "tf-az-vnet-peering-2" {
+resource "azurerm_virtual_network" "vnet2" {
   name                = "terraform-azurerm-virtual-network-peering-vnet2"
-  resource_group_name = azurerm_resource_group.tf-az-vnet-peering.name
-  location            = azurerm_resource_group.tf-az-vnet-peering.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.2.0.0/16"]
 }
 
-resource "azurerm_virtual_network" "tf-az-vnet-peering-3" {
+resource "azurerm_virtual_network" "vnet3" {
   name                = "terraform-azurerm-virtual-network-peering-vnet3"
-  resource_group_name = azurerm_resource_group.tf-az-vnet-peering.name
-  location            = azurerm_resource_group.tf-az-vnet-peering.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.3.0.0/16"]
 }
 
-module "tf-az-vnet-peering-1" {
-  source               = "./terraform-azurerm-virtual-network-peering"
-  resource_group_name  = azurerm_virtual_network.tf-az-vnet-peering-1.resource_group_name
-  virtual_network_name = azurerm_virtual_network.tf-az-vnet-peering-1.name
+module "peering1" {
+  source               = "aztfm/virtual-network-peering/azurerm"
+  version              = ">=1.0.0"
+  resource_group_name  = azurerm_virtual_network.vnet1.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   peerings = [
-    { name = azurerm_virtual_network.tf-az-vnet-peering-2.name, remote_virtual_network_id = azurerm_virtual_network.tf-az-vnet-peering-2.id },
-    { name = azurerm_virtual_network.tf-az-vnet-peering-3.name, remote_virtual_network_id = azurerm_virtual_network.tf-az-vnet-peering-3.id }
+    { name = azurerm_virtual_network.vnet2.name, remote_virtual_network_id = azurerm_virtual_network.vent2.id },
+    { name = azurerm_virtual_network.vnet3.name, remote_virtual_network_id = azurerm_virtual_network.vent3.id }
   ]
 }
 
-module "tf-az-vnet-peering-2" {
-  source               = "./terraform-azurerm-virtual-network-peering"
-  resource_group_name  = azurerm_virtual_network.tf-az-vnet-peering-2.resource_group_name
-  virtual_network_name = azurerm_virtual_network.tf-az-vnet-peering-2.name
+module "peering2" {
+  source               = "aztfm/virtual-network-peering/azurerm"
+  version              = ">=1.0.0"
+  resource_group_name  = azurerm_virtual_network.vnet2.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
   peerings = [
-    { name = azurerm_virtual_network.tf-az-vnet-peering-1.name, remote_virtual_network_id = azurerm_virtual_network.tf-az-vnet-peering-1.id },
-    { name = azurerm_virtual_network.tf-az-vnet-peering-3.name, remote_virtual_network_id = azurerm_virtual_network.tf-az-vnet-peering-3.id }
+    { name = azurerm_virtual_network.vnet1.name, remote_virtual_network_id = azurerm_virtual_network.vnet1.id },
+    { name = azurerm_virtual_network.vnet3.name, remote_virtual_network_id = azurerm_virtual_network.vnet3.id }
   ]
 }
 
-module "tf-az-vnet-peering-3" {
-  source               = "./terraform-azurerm-virtual-network-peering"
-  resource_group_name  = azurerm_virtual_network.tf-az-vnet-peering-3.resource_group_name
-  virtual_network_name = azurerm_virtual_network.tf-az-vnet-peering-3.name
+module "peering3" {
+  source               = "aztfm/virtual-network-peering/azurerm"
+  version              = ">=1.0.0"
+  resource_group_name  = azurerm_virtual_network.vnet3.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet3.name
   peerings = [
-    { name = azurerm_virtual_network.tf-az-vnet-peering-1.name, remote_virtual_network_id = azurerm_virtual_network.tf-az-vnet-peering-1.id },
-    { name = azurerm_virtual_network.tf-az-vnet-peering-2.name, remote_virtual_network_id = azurerm_virtual_network.tf-az-vnet-peering-2.id }
+    { name = azurerm_virtual_network.vnet1.name, remote_virtual_network_id = azurerm_virtual_network.vnet1.id },
+    { name = azurerm_virtual_network.vnet2.name, remote_virtual_network_id = azurerm_virtual_network.vnet2.id }
   ]
 }
